499cf9fc63c34d9f7b990a3f0d96b5e02cd9cdae
IevaRugine <70884312+IevaRugine@users.noreply.github.com>
2020-10-05 14:01:27 +0300

    content Text

---
+++ js/renderContentText.js
@@ -0,0 +1,18 @@
+function renderContentText(data) {
+   const shortTextLength = 60;
+   const normalTextLength = 300;
+   const largeTextLenth = 1000;
+   const textLength = text.lentgh;
+//kai labai mazai teksto - padidint srifta;
+//kai normaliai teksto - tiesiog atvaziduojam;
+// kai teksto labai daug - nukerpam ir pridedam ".... See more"
+//o jei teksto daugiau nei leidziama maximaliai - rodom klaida
+
+
+
+   return `<p>${text}</p>`;
+
+
+}
+
+export default renderContentText;
