28584540a22ff267e8f16a72d7f2f70a677588ae
IevaRugine <70884312+IevaRugine@users.noreply.github.com>
2020-10-05 15:10:53 +0300

    Content Text

--- js/renderContentText.js
+++ js/renderContentText.js
@@ -1,9 +1,30 @@
-function renderContentText(data) {
+function renderContentText(text) {
+   const shortTextLength = 60;
+   const normalTextLength = 300;
+   const largeTextLength = 10000;
+   const textLength = text.length;
+   
+//kai labai mazai teksto - padidint srifta;
+if (textLength <= shortTextLength) {
+   return `<p class="big-font">${text}</p>`;
 
-   return data;
-// `<div class="post-content">
-//     <p>TEKSTAS</p>
-// </div>`;
+   } else if (textLength <= normalTextLength) {
+  //kai normaliai teksto - tiesiog atvaziduojam; 
+   return `<p>${text}</p>`;
+
+   } else if (textLength <= largeTextLength) {
+   // kai teksto labai daug - nukerpam ir pridedam ".... See more"    
+   let shorterText = '';
+   for (let i = 0; i < normalTextLength; i++) {
+      shorterText += text[i];
+   }
+      return `<p>${shorterText} + <span class="see-more">...See more </span></p>`;
+   
+   } else {
+   //o jei teksto daugiau nei leidziama maximaliai - rodom klaida
+   console.warn(`Error: virsytas leistinas maksimalus ${largeTextLength} teksto ilgis`);
+   return '';
+ }
 
 }
 
